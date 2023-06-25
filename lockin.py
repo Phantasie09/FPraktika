ec0a136d3b81fce6715847e9493133479ff8c9e4
Kangee02 <137641028+Kangee02@users.noreply.github.com>
2023-06-25 14:36:17 +0200

    Add files via upload
    Legende hinzugefügt

--- lockin.py
+++ lockin.py
@@ -11,7 +11,7 @@ at = [1, 0.333, 0.200, 0.143, 0.111, 0.091, 0.079, 0.067, 0.059]
 am = [1, 0.355, 0.200, 0.141, 0.112, 0.089, 0.079, 0.071, 0.056]
 f = [1, 3, 5, 7, 9, 11, 13, 15, 17]
 
-t= np.linspace(0, 2, num=1000)
+t= np.linspace(0, 1.5, num=1000)
 
 def ft(n,x):
     y = (4/np.pi)*at[n]*np.sin(2*np.pi*f[n]*x)
@@ -23,10 +23,11 @@ def fm(n,x):
     return z
 
 
-plt.plot(t, sum(ft(n,t) for n in range(0,8)))
+plt.plot(t, sum(ft(n,t) for n in range(0,8)), label='theoretisch')
 plt.xlabel("t/s")
 plt.ylabel("R(t)/V")
-plt.plot(t, sum(fm(n,t) for n in range(0,8)))
+plt.plot(t, sum(fm(n,t) for n in range(0,8)), label='gemessen')
+plt.legend(loc='lower right')
 plt.show()
 
 
