c7673557fcad0dbe9d9861ae8644a22d5d3ebe60
Phantasie09 <mitplayer03@gmail.com>
2023-07-06 08:31:17 +0200

    tesz
    keine a

--- OpticsPlasma/dataread.py
+++ OpticsPlasma/dataread.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-arbeit=np.load('G:/Git/FPraktika/OpticsPlasma/F407_backup/my_array_temp.npy')
+#arbeit=np.load('G:/Git/FPraktika/OpticsPlasma/F407_backup/my_array_temp.npy')
 t=np.loadtxt('../OpticsPlasma/F407_backup/7.05 TimKatharine/2.1.10pa.asc')
 #f= open("/F407_backup/test.asc")
 #ascii_grid = np.loadtxt("OpticsPlasma\\F407_backup\\test.asc", skiprows=6)
